0a02b4077735a54490772ff3a42fc10f2bba2152
tera haz <craphazzard@gmail.com>
2019-05-18 00:15:02 -0400

    slack commands are no go, exclamation point commands are go tho

--- features/commands.js
+++ features/commands.js
@@ -5,20 +5,9 @@
 
 module.exports = function(controller) {
 
-    // slash command handler
-    controller.on('slash_command', async(bot, message) => {
-
-        if (message.text === 'plain') {
-            await bot.reply(message, 'This is a plain reply');
-        } else if (message.text === 'public') {
-            await bot.replyPublic(message, 'This is a public reply');
-        } else if (message.text === 'private') {
-            await bot.replyPrivate(message, 'This is a private reply');
-        }
-
-        // set http status
-        bot.httpBody({text:'You can send an immediate response using bot.httpBody()'});
-
+    controller.hears(/^!\w+/, ['message','direct_message'] async function (bot, message) {
+        var c = /^!(\w+) ?(.*)$/.matchAll(message.text);
+        await bot.replyEphemeral(message, c[1]);
     });
 
     function getCommand(message) {
