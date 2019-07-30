e34eb90acc71d951a72b445c0d5161d009847c81
tera haz <craphazzard@gmail.com>
2019-07-30 00:45:33 -0400

    naturally people will break this so lets defend against that preemtively

--- features/weather.js
+++ features/weather.js
@@ -19,120 +19,32 @@ module.exports = function(controller) {
         let response = await fetch(`${uri}?q=${str}&APPID=${process.env.openWeatherApiKey}&units=metric`);
         let data = await response.json();
 
-        let far = data.main.temp * (9/5) + 32;
-        let text = `${data.name} - ${data.weather[0].description}\n\n*${data.main.temp}ºC / ${far}ºF*\n ${data.main.humidity}% humidity`;
-        let image_url = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-        let alt_text = `${data.weather[0].main}`;
+        if (data.cod != "200") {
+            await bot.reply(message, {text: data.message });
+        } else {
 
-        await bot.reply(message,{
-            blocks: [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": text
-                    },
-                    "accessory": {
-                        "type": "image",
-                        "image_url": image_url,
-                        "alt_text": alt_text
-                    }
-                }
-            ]
-        });
+            let far = data.main.temp * (9/5) + 32;
+            let text = `${data.name} - ${data.weather[0].description}\n\n*${data.main.temp}ºC / ${far}ºF*\n ${data.main.humidity}% humidity`;
+            let image_url = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+            let alt_text = `${data.weather[0].main}`;
 
-    });
-
-
-    // example sending formatted block elements including buttons.
-    controller.hears('blocks', 'message', async(bot, message) => {
-
-        await bot.reply(message,{
-            blocks: [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Hello, Assistant to the Regional Manager Dwight! *Michael Scott* wants to know where you'd like to take the Paper Company investors to dinner tonight.\n\n *Please select a restaurant:*"
-                    }
-                },
-                {
-                    "type": "divider"
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "*Farmhouse Thai Cuisine*\n:star::star::star::star: 1528 reviews\n They do have some vegan options, like the roti and curry, plus they have a ton of salad stuff and noodles can be ordered without meat!! They have something for everyone here"
-                    },
-                    "accessory": {
-                        "type": "image",
-                        "image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/c7ed05m9lC2EmA3Aruue7A/o.jpg",
-                        "alt_text": "alt text for image"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "*Kin Khao*\n:star::star::star::star: 1638 reviews\n The sticky rice also goes wonderfully with the caramelized pork belly, which is absolutely melt-in-your-mouth and so soft."
-                    },
-                    "accessory": {
-                        "type": "image",
-                        "image_url": "https://s3-media2.fl.yelpcdn.com/bphoto/korel-1YjNtFtJlMTaC26A/o.jpg",
-                        "alt_text": "alt text for image"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "*Ler Ros*\n:star::star::star::star: 2082 reviews\n I would really recommend the  Yum Koh Moo Yang - Spicy lime dressing and roasted quick marinated pork shoulder, basil leaves, chili & rice powder."
-                    },
-                    "accessory": {
-                        "type": "image",
-                        "image_url": "https://s3-media2.fl.yelpcdn.com/bphoto/DawwNigKJ2ckPeDeDM7jAg/o.jpg",
-                        "alt_text": "alt text for image"
-                    }
-                },
-                {
-                    "type": "divider"
-                },
-                {
-                    "type": "actions",
-                    "elements": [
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "Farmhouse",
-                                "emoji": true
-                            },
-                            "value": "Farmhouse"
+            await bot.reply(message,{
+                blocks: [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": text
                         },
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "Kin Khao",
-                                "emoji": true
-                            },
-                            "value": "Kin Khao"
-                        },
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "Ler Ros",
-                                "emoji": true
-                            },
-                            "value": "Ler Ros"
+                        "accessory": {
+                            "type": "image",
+                            "image_url": image_url,
+                            "alt_text": alt_text
                         }
-                    ]
-                }
-            ]
-        });
+                    }
+                ]
+            });
+        }
 
     });
-
 }
