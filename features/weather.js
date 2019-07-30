32ba21d45ece4d8aded60f0f4ca7b22b721297af
tera haz <craphazzard@gmail.com>
2019-07-29 22:59:19 -0400

    block formatting for weather.js

--- features/weather.js
+++ features/weather.js
@@ -16,10 +16,31 @@ module.exports = function(controller) {
         tokens.shift();
         let str = tokens.join(' ').trim();
 
-        let response = await fetch(`${uri}?q=${str}&APPID=${process.env.openWeatherApiKey}`);
+        let response = await fetch(`${uri}?q=${str}&APPID=${process.env.openWeatherApiKey}&units=metric`);
         let data = await response.json();
 
-        await bot.reply(message,{ text: data });
+
+
+        await bot.reply(message,{
+            blocks: [
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": `Weather for *${data.name}*\n${data.temperature.value}ºC ${data.weather.description}\n`
+                    },
+                    "accessory": {
+                        "type": "image",
+                        "image_url": `http://openweathermap.org/img/wn/${data.weather.icon}@2x.png`,
+                        "alt_text": `${data.weather.main}`
+                    }
+                }
+            ]
+        });
+
     });
 
 }
