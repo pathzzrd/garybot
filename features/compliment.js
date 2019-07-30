2ba69b1b30f71431052d2b8f09fbcbae32090385
Sean Padden <you@example.com>
2019-07-30 10:30:45 -0400

    Added a compliment feature and also edited the copyright text in weather dont worry i didnt touch the code just being stupid

---
+++ features/compliment.js
@@ -0,0 +1,29 @@
+// copyright idk why i didn't get the ms copyright text
+// (c) what am i doing wrong lol 2019
+
+
+
+
+module.exports = function(controller){
+
+  const fetch = require('node-fetch');
+
+  const uri = "https://complimentr.com/api"
+
+  // !compliment
+  controller.hears(/^!compliment/i ['message', 'direct_message'], async function(bot, message) {
+    // removes command
+    let tokens = message.text.split(' ')
+    tokens.shift();
+    let str = tokens.join(' ');
+
+    let response = await fetch(`${uri}`)
+    let data = await response.json();
+    let compliment = `${data.compliment}`
+
+    await bot.reply(message, compliment)
+  })
+}
+
+
+
--- features/weather.js
+++ features/weather.js
@@ -1,7 +1,7 @@
 
 /**
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
+ * copyright (c) my diq 
+ * licensed under the  my bolls.
  */
 module.exports = function(controller) {
 
