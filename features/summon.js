0bf164f68a87c4b039589bbbeee929904a6b626b
tera haz <craphazzard@gmail.com>
2019-08-12 13:26:53 -0400

    MVP for summon command

---
+++ features/summon.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+module.exports = function(controller) {
+
+    const fetch = require('node-fetch');
+    const jsdom = require('jsdom');
+    const { JSDOM } = jsdom;
+
+    const useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
+
+    // !summon
+    controller.hears(/^!summon/i, ['message','direct_message'], async function(bot, message) {
+        // removes command
+        let tokens = message.text.split(' ')
+        tokens.shift();
+        let str = tokens.join(' ');
+
+        let response = await fetch ("https://www.google.com/search?q=beans&tbm=isch", { headers: {"User-Agent": useragent }})
+        let html = await response.text()
+        dom = new JSDOM(html)
+        let imageurl = JSON.parse(dom.window.document.getElementsByClassName('rg_meta').item(0).innerHTML).ou;
+
+        await bot.reply(message,{ text: imageurl });
+    });
+
+}
